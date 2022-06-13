4f03fba33d365bf4c1c7592bcc795caddf291712
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-06-13 14:45:11 -0300

    commit

---
+++ src/RegisterAdmin.java
@@ -0,0 +1,28 @@
+
+public class RegisterAdmin {
+    private String login;
+    private String password;
+
+    public RegisterAdmin(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
